c7a500cdba5197771f0f78114aa9cbdbb829d5c3
Ricardo <ramartinez@sati-ca.com>
2016-11-22 01:58:54 -0500

    Ajuste a archivo py de configuracion de grabaciones

--- paso_100_TARJETA_CANAL_G.py
+++ paso_100_TARJETA_CANAL_G.py
@@ -6,9 +6,6 @@ import subprocess
 import time
 import datetime
 import shutil
-from clsGrabacion import clsGrabacion
-from clsGrabacion import clsParametros
-from clsGrabacion import clsUtilidades
 
 # formatos de fecha
 format_date = "%Y%m%d"
@@ -22,7 +19,7 @@ NombrePrograma ='paso_grabacion_v2'
 
 # variables de Canal y tarjeta
 # son obtenidas del mismo nombre del archivo
-# el archivo debe venir en el formato paso_100_TARJETA_Canal_G.py
+# el archivo debe venir en el formato paso_100_TARJETA_Canal_Medio_G.py
 NombreScript = os.path.basename(__file__)
 detalles = NombreScript.split('_')
 Canal=detalles[3]
@@ -40,23 +37,18 @@ os.system(cmd)
 cmd = 'ivtv-tune -c' + Canal + ' -d ' + NombreTarjeta
 os.system(cmd)
 
-# si es menos de las 5 AM no se graba
-reporter = clsUtilidades()
-reportLog = reporter.ReportStatus(Mensaje='Iniciando Grabacion de Canal' + Canal ,IDComputadora=parametros.IDComputadora,NombrePrograma=NombrePrograma,IDPais=parametros.IDPais,IDCanal=Medio,Metodo=0,Tiempo=0,FechaHoraArchivo=datetime.datetime.now().strftime(format_datetimeU),Codigo=100,IDRegistro=0)
+
 
 hora = datetime.datetime.now().hour
 minuto = datetime.datetime.now().minute
 
     
 #grabamos
-Duracion=3570
+Duracion=3590
 NombreArchivo = Medio + "_" + datetime.datetime.now().strftime(format_datetime) + ".mpg"
-g = clsGrabacion()
-g.get_ObjectFromFileName(NombreArchivo, parametros.IDPais)
 NombreArchivo = os.path.join(DirectorioGrabacion,NombreArchivo)
 
 print 'Si hay proceso de grabacion activo en vlc , lo eliminaremos para evitar posibles conflicos'
-util = clsUtilidades()
 cadena = "pvr:///dev/video" + str(detalles[2])
 util.killProcess(cadena)
 
@@ -65,15 +57,13 @@ time.sleep(5)
 print 'Grabaremos el archivo ' + NombreArchivo + ' por una duracion de ' + str(Duracion)
 HoraGrabacion = datetime.datetime.now().strftime(format_datetimeU)
 HoraI = datetime.datetime.now()
-reportLog = reporter.ReportStatus(Mensaje='Iniciando Grabacion de Hora' + str(hora) ,IDComputadora=parametros.IDComputadora,NombrePrograma=NombrePrograma,IDPais=parametros.IDPais,IDCanal=Canal,Metodo="grabacion",Tiempo=0,FechaHoraArchivo=HoraGrabacion,Codigo=101,IDRegistro=g.IDRegistro)       
+
 cmd = 'cvlc pvr://{0} :norm=ntsc :size=720x480 :bitrate=3000000 :maxbitrate=4000000 :run-time={1} --cr-average 1000 --sout=file/ps://{2} vlc://quit'.format(NombreTarjeta,Duracion,NombreArchivo)
+
+cmd = "cvlc 'v4l2c://{0}:audio-method=0:controls-reset:set-ctrls={video_bitrate_mode=1,video_bitrate=4000000,video_peak_bitrate=4000000}:width=720:height=480' --run-time={1}  --sout=file/ps://{2} vlc://quit".format(NombreTarjeta,Duracion,NombreArchivo)
+
+print cmd
 subprocess.call(cmd, shell=True)
 print 'Grabacion finalizada'
 tDiff= datetime.datetime.now() - HoraI
-reportLog = reporter.ReportStatus(Mensaje='Terminada la Grabacion de Hora' + str(hora) ,IDComputadora=parametros.IDComputadora,NombrePrograma=NombrePrograma,IDPais=parametros.IDPais,IDCanal=Canal,Metodo="grabacion",Tiempo=tDiff.seconds,FechaHoraArchivo=HoraGrabacion,Codigo=105,IDRegistro=g.IDRegistro)
-    
-
-
-
-
 
