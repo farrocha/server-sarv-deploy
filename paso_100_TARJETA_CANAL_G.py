1b4dd4a9f9962a329af06c9b3a27dbbd2a348f6e
Ricardo <ramartinez@sati-ca.com>
2016-11-22 13:21:16 -0500

    Cambios en el paso_100 python

--- paso_100_TARJETA_CANAL_G.py
+++ paso_100_TARJETA_CANAL_G.py
@@ -7,6 +7,10 @@ import time
 import datetime
 import shutil
 
+from clsGrabacion import clsGrabacion
+from clsGrabacion import clsParametros
+from clsGrabacion import clsUtilidades
+
 # formatos de fecha
 format_date = "%Y%m%d"
 format_time ="%H%M%S"
@@ -49,10 +53,11 @@ NombreArchivo = Medio + "_" + datetime.datetime.now().strftime(format_datetime) 
 NombreArchivo = os.path.join(DirectorioGrabacion,NombreArchivo)
 
 print 'Si hay proceso de grabacion activo en vlc , lo eliminaremos para evitar posibles conflicos'
-cadena = "pvr:///dev/video" + str(detalles[2])
+util.killProcess(cadena)
+cadena = "v4l2:///dev/video" + str(detalles[2])
 util.killProcess(cadena)
 
-time.sleep(5)
+time.sleep(3)
 
 print 'Grabaremos el archivo ' + NombreArchivo + ' por una duracion de ' + str(Duracion)
 HoraGrabacion = datetime.datetime.now().strftime(format_datetimeU)
@@ -60,7 +65,7 @@ HoraI = datetime.datetime.now()
 
 cmd = 'cvlc pvr://{0} :norm=ntsc :size=720x480 :bitrate=3000000 :maxbitrate=4000000 :run-time={1} --cr-average 1000 --sout=file/ps://{2} vlc://quit'.format(NombreTarjeta,Duracion,NombreArchivo)
 
-cmd = "cvlc 'v4l2c://{0}:audio-method=0:controls-reset:set-ctrls={video_bitrate_mode=1,video_bitrate=4000000,video_peak_bitrate=4000000}:width=720:height=480' --run-time={1}  --sout=file/ps://{2} vlc://quit".format(NombreTarjeta,Duracion,NombreArchivo)
+cmd = 'cvlc \'v4l2c://{0}:audio-method=0:controls-reset:set-ctrls={{video_bitrate_mode=1,video_bitrate=4000000,video_peak_bitrate=4000000}}:width=720:height=480\' --run-time={1}  --sout=file/ps://{2} vlc://quit'.format(NombreTarjeta,Duracion,NombreArchivo)
 
 print cmd
 subprocess.call(cmd, shell=True)
