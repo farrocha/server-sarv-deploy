5891420fcf670078d8b67eeaa596d57e5995b4da
farrocha <ramartinez@sati-ca.com>
2019-04-16 22:47:50 -0400

    Update script_2.sh
    Ajustes para Ubunt18 y docker

--- scripts_instalacion/script_2.sh
+++ scripts_instalacion/script_2.sh
@@ -1,14 +1,14 @@
 echo "Inicio de instalacion de librerias..."
 sudo apt-get update
 
-sudo apt-get --yes install mono-complete
+#sudo apt-get --yes install mono-complete
 sudo apt-get --yes install build-essential libavformat-dev libmpg123-dev libsamplerate-dev libsndfile-dev cimg-dev libavcodec-dev  libswscale-dev
 sudo apt-get --yes install libpng-dev libjpeg-dev libopencv-dev git unzip libargtable2-dev libmagickwand-dev imagemagick vlc python-dev python-pip 
 sudo apt-get --yes install patchutils libproc-processtable-perl
-sudo apt-get --yes install v4l-utils
-sudo apt-get --yes install ivtv-utils
-sudo apt-get --yes install mplayer
-sudo apt-get --yes install filezilla
+#sudo apt-get --yes install v4l-utils
+#sudo apt-get --yes install ivtv-utils
+#sudo apt-get --yes install mplayer
+#sudo apt-get --yes install filezilla
 sudo apt-get --yes install vsftpd
 sudo apt-get --yes install libx264-dev
 sudo apt-get --yes install libmp3lame-dev
@@ -17,9 +17,9 @@ sudo apt-get --yes  install ffmpeg
 
 sudo apt-get update
 
-sudo apt-get --yes install apache2
-sudo apt-get --yes install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi  php7.0 
-sudo apt-get --yes install libapache2-mod-php
+#sudo apt-get --yes install apache2
+#sudo apt-get --yes install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi  php7.0 
+#sudo apt-get --yes install libapache2-mod-php
 
 
 
@@ -32,10 +32,10 @@ echo ""
 
 pip install boto
 pip install psutil
-pip install python-openstackclient
-pip install python-swiftclient
-pip install django
-pip install --upgrade watson-developer-cloud
+#pip install python-openstackclient
+#pip install python-swiftclient
+#pip install django
+#pip install --upgrade watson-developer-cloud
 
 
 echo "PROGRAMAS PARA RADIO"
